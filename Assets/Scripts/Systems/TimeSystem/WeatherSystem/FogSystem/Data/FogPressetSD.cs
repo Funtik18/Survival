59844f8df9f81e1da342f6fac2534690793d2744
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2021-05-05 23:02:44 +0700

    Add fog

---
+++ Assets/Scripts/Systems/TimeSystem/WeatherSystem/FogSystem/Data/FogPressetSD.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game/Environment/FogPresset", fileName = "Data")]
+public class FogPressetSD : ScriptableObject
+{
+    public Color fogColor;
+    [Range(0, 1f)] 
+    public float rangeValue;
+}
